b2e6b1dbeec8ac337215d0027c6e14abeed31021
Oded Galor <odedgalor@Odeds-MBP.fios-router.home>
2017-03-04 13:22:59 -0500

    Completed BinaryGap

---
+++ BinaryGap.py
@@ -0,0 +1,16 @@
+def solution(N):
+    
+    string = bin(N)[2:]
+    
+    max = 0
+    count = 0
+    
+    for i in string:
+        if i=='0':
+            count += 1
+        else:
+            if count > max:
+                max = count
+            count = 0
+    
+    return max
