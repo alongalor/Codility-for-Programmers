5732092ac2dd016357ef358263aa31b330832dd4
Alon Galor <agalor@hbs.edu>
2017-03-05 22:50:08 -0500

    Updated OddOccurencesInArray

--- OddOccurencesInArray.py
+++ OddOccurencesInArray.py
@@ -1,13 +1,2 @@
 def solution(A):
-    d = {}
-    for i in A:
-        d[i] = d.get(i, 0) + 1
-
-    spec_val = 0
-    for i in d.values():
-        if i%2 != 0:
-            spec_val = i
-            
-    for key, val in list(d.iteritems()):
-        if val == spec_val:
-            return key
+    return reduce(lambda x, y: x ^ y, A)
