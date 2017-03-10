e183aabc592ad68ee06b00a101ccb4fc30fbd3b5
Alon Galor <agalor@hbs.edu>
2017-03-09 19:55:54 -0500

    Uploaded Nesting

---
+++ Nesting.py
@@ -0,0 +1,9 @@
+def solution(S):
+    A = ['']
+    for i in xrange(len(S)):
+        A += [S[i]]
+        if A[-2:] == ["(",")"]:
+            A = A[:-2]
+    if len(A)==1:
+        return 1
+    return 0
