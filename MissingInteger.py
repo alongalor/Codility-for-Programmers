f8946bc2e23c1bc97ce6ebce45295b68fe7dbbc8
Alon Galor <agalor@hbs.edu>
2017-05-09 15:38:02 -0400

    Update MissingInteger.py

--- MissingInteger.py
+++ MissingInteger.py
@@ -1,9 +1,12 @@
 def solution(A):
     lst = [0]*len(A)
+    
     for i in A:
-        if 1<=i<=len(A):
+        if 1 <= i <= len(A):
             lst[i-1] = 1
-    for i in xrange(len(lst)+1):
-        if lst[i]==0:
+    
+    for i in xrange(len(lst)):
+        if lst[i] == 0:
             return i+1
+        
     return len(A)+1
