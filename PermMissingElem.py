1b43f975a3ebc8dcda45ba2b9e94bae992670854
Alon Galor <agalor@hbs.edu>
2017-05-09 14:36:08 -0400

    Updated PermMissingElem

--- PermMissingElem.py
+++ PermMissingElem.py
@@ -1,4 +1,9 @@
 def solution(A):
+    return reduce(lambda x,y: x + y, range(len(A) + 2)) - sum(A)
+
+# Alternative Solution
+
+def solution(A):
     lst = [0]*(len(A)+1)
     for i in A:
         lst[i-1] = 1
